9b013f40cb8e64e09abb852d50035f22474ad76e
Lea Model <lea.skywalker309@gmail.com>
2023-08-27 19:35:59 +0200

    Tech Challenge finished

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,35 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
+const api_url = "http://localhost:5005";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [allPhones, setAllPhones] = useState([]);
+  const [onePhone, setOnePhone] = useState();
+
+  useEffect(() => {
+    const getPhones = async () => {
+      try {
+        const { data } = await axios.get(`${api_url}/phones`);
+        if (data) {
+          setAllPhones(data);
+          console.log("phones: ", data);
+        } else {
+          ("no success");
+        }
+      } catch (error) {
+        console.log("Error fetching all phones: ", error);
+      }
+    };
+    getPhones();
+  }, []);
+
+  // using axios due to API. A simpler method would be to setState of onePhone directly with the already available phone data
+  const handlePhoneSelection = async (phoneId) => {
+    try {
+      const { data } = await axios.get(`${api_url}/phones/${phoneId}`);
+      if (data) {
+        setOnePhone(data);
+        console.log("phone: ", data);
+      } else {
+        ("no success");
+      }
+    } catch (error) {
+      console.log("Error fetching all phones: ", error);
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <h1>Discover our Phones</h1>
+      <div className="content">
+        <div className="phones-list">
+          {allPhones ? (
+            allPhones.map((phone) => (
+              <div key={phone.id} className="phone-item">
+                <h3 onClick={() => handlePhoneSelection(phone.id)}>
+                  {phone.name}
+                </h3>
+              </div>
+            ))
+          ) : (
+            <h1>Loading...</h1>
+          )}
+        </div>
+        {onePhone && (
+          <div className="phone-details" key={onePhone.id}>
+            <h2>
+              {onePhone.name} in {onePhone.color} at ${onePhone.price}
+            </h2>
+            <img
+              src={`../assets/images/${onePhone.imageFileName}`}
+              alt="Picture of Phone"
+              style={{ height: "150px" }}
+            />
+            <h3>Manufacturer: {onePhone.manufacturer}</h3>
+            <h3>Screen Size: {onePhone.screen}</h3>
+            <h3>Processor: {onePhone.processor}</h3>
+            <h3>Ram: {onePhone.ram}</h3>
+            <p>{onePhone.description}</p>
+            <button onClick={() => setOnePhone(null)}>Close</button>
+          </div>
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
